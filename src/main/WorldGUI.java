9ed78d04cab30264fbed7bc98d25bc0797623373
Effey46 <afagot92@gmail.com>
2018-01-27 14:06:43 +0100

    cell gui works

--- src/main/WorldGUI.java
+++ src/main/WorldGUI.java
@@ -2,19 +2,22 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class WorldGUI {
+public class WorldGUI{
 	public WorldGUI(){
+		
 		World world=new World();
+		
 		JFrame frame=new JFrame("Let's split up alpha 0.0");
 		frame.setMinimumSize(new Dimension(1000,1000));
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		
 		int i = 40;
 		int j = 40;
-		JPanel[][] panelHolder = new JPanel[i][j];
 		GridLayout gdl=new GridLayout(i,j);
-		frame.setLayout(gdl);
+		JPanel[][] panelHolder = new JPanel[i][j];
+		
 		for(int m = 0; m < i; m++) {
 		   for(int n = 0; n < j; n++) {
 		      panelHolder[m][n] = new JPanel();
@@ -23,11 +26,18 @@ public class WorldGUI {
 		   }
 		}
 		for(int m = 0; m < i; m++) {
-			   for(int n = 0; n < j; n++) {
-				  int k=(int)(Math.random()*255); 
-			      panelHolder[m][n].setBackground(new Color(255,255-k,255-k));
-			   }
-			}
+		   for(int n = 0; n < j; n++) {
+			  int k=(int)(Math.random()*255); 
+		      panelHolder[m][n].setBackground(new Color(255,255-k,255-k));
+		   }
+		}
+		final JPanel cell = (JPanel) frame.getGlassPane();
+		cell.setPreferredSize(new Dimension(1000,1000));
+		cell.setLayout(new BorderLayout());
+		cell.add(new CellGUI());
+		cell.setVisible(true);
+		
+		frame.setLayout(gdl);
 		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
